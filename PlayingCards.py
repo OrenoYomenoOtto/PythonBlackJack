c18e9d23d248df27c778ad66b44f5962292d3d8e
C0B22174 <c0b22174ac@edu.teu.ac.jp>
2024-11-24 21:45:49 +0900

    複数のクラスを追加 - FaceCard クラス - Joker クラス - Card クラス

--- PlayingCards.py
+++ PlayingCards.py
@@ -1,9 +1,32 @@
 # coding:utf-8
 
-from enum import Enum, auto
+from typing import Final
+from enum import Enum, IntEnum, auto
 
 class Suit(Enum):
-    SPADE = auto()
-    CLUB = auto()
-    HEART = auto()
-    DIAMOND = auto()
+    SPADE: Final[int] = auto()
+    CLUB: Final[int] = auto()
+    HEART: Final[int] = auto()
+    DIAMOND: Final[int] = auto()
+
+class FaceCard(IntEnum):
+    ACE = 1
+    JACK: Final[int] = 11
+    QUEEN: Final[int] = 12
+    KING: Final[int] = 13
+
+class Joker:
+    JOKER = None
+
+class Card:
+    def __init__(self, rank: int, suit: Suit) -> None:
+        self.__rank = rank
+        self.__suit = suit
+
+    @property
+    def get_rank(self) -> int:
+        return self.__rank
+    
+    @property
+    def get_suit(self) -> Suit:
+        return self.__suit
