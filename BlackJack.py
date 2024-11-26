40a696cd317df959b0f8bb0a5dc93a50f14e1f94
C0B22174 <c0b22174ac@edu.teu.ac.jp>
2024-11-26 10:32:01 +0900

    main関数のコード追加

--- BlackJack.py
+++ BlackJack.py
@@ -1,6 +1,5 @@
 #coding:utf-8
 
-import random
 import enum
 import configparser
 from typing import Final, Tuple, List
@@ -10,9 +9,11 @@ import PlayingCards as Cards
 
 
 INITIAL_CHIP: Final[int] = 100
+DEALER_STAND_NUMBER: Final[int] = 17
 BLACK_JACK_NUMBER: Final[int] = 21
 MIN_RATE: Final[int] = 10
 MAX_RATE: Final[int] = 100
+PLAYER_NUM: Final[int] = 2
 
 
 class Rule():
@@ -69,7 +70,31 @@ class Dealer(Rule):
 
 
 def main():
+    #各クラスのインスタンス化
     Deck = Cards.Deck()
+    Dealer = Dealer()
+    Players = [Player() for i in range(PLAYER_NUM)]
+
+    #カードを配る
+    for i in range(2):
+        Dealer.hit()
+        for Player in Players:
+            Player.hit()
+    #プレイヤーの行動
+    while True:
+        for Player in Players:
+            #TODO次の行動を決める
+            pass
+        #player全員がstandもしくはburstしてるか確認する
+        counter = 0
+        for Player in Players:
+            if Player.get_isStand is True or Player.get_isBurst is True:
+                counter += 1
+        if counter == PLAYER_NUM:
+            break
+    #ディーラーの行動
+    while True:
+        pass
 
 
 if __name__ == "__main__":
