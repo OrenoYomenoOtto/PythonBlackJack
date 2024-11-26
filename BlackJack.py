c4c47362e4c0ff022b53102d95a8df68051338ab
C0B22174 <c0b22174ac@edu.teu.ac.jp>
2024-11-26 09:14:30 +0900

    Margeに伴う修正 - BlackJackモジュール内のトランプ関連クラスを削除 - Ruleクラスでトランプ関連の記述を削除 その他修正 - main関数の追加

--- BlackJack.py
+++ BlackJack.py
@@ -6,58 +6,13 @@ import configparser
 from typing import Final, Tuple, List
 from abc import ABCMeta, abstractmethod
 
+import PlayingCards as Cards
+
 
 INITIAL_CHIP: Final[int] = 100
-BLACK_JACK: Final[int] = 21
-MIN_ACE: Final[int] = 1
-MAX_ACE: Final[int] = 11
+BLACK_JACK_NUMBER: Final[int] = 21
 MIN_RATE: Final[int] = 10
 MAX_RATE: Final[int] = 100
-
-class InitialCard(enum.IntEnum):
-    A = 11
-    J = 10
-    Q = 10
-    K = 10
-
-
-class Suit(enum.Enum):
-    club = enum.auto()
-    diamond = enum.auto()
-    heart = enum.auto()
-    spade = enum.auto()
-
-
-class Card:
-    def __init__(self, number: int, suit: int):
-        self.__number = number
-        self.__suit = suit
-
-    @property
-    def get_number(self):
-        return self.__number
-    
-    @property
-    def get_suit(self):
-        return self.__suit
-
-
-class Deck:
-    CARD_NUMBER: Tuple = (InitialCard.A, 2, 3, 4, 5, 6, 7, 8, 9, 10, InitialCard.J, InitialCard.Q, InitialCard.K)
-    CLUB_CARD: Tuple = (Card(number, Suit.club) for number in CARD_NUMBER)
-    DIAMOND_CARD: Tuple = (Card(number, Suit.diamond) for number in CARD_NUMBER)
-    HEART_CARD: Tuple = (Card(number, Suit.heart) for number in CARD_NUMBER)
-    SPADE_CARD: Tuple = (Card(number, Suit.spade) for number in CARD_NUMBER)
-
-    def __init__(self):
-        self.deck = list(Deck.CLUB_CARD) +list(Deck.DIAMOND_CARD) +list(Deck.HEART_CARD) +list(Deck.SPADE_CARD)
-
-    def shuffle_deck(self) -> None:
-        random.shuffle(self.deck)
-
-    def pull_card(self) -> int:
-        pulled_card = self.deck.pop(0)
-        return pulled_card
 
 
 class Rule():
@@ -72,49 +27,28 @@ class Rule():
     @property
     def get_hand(self) -> list:
         return self.__hand
-    
-    @property
-    def get_total(self) -> int:
-        return self.__total
-    
+
     @property
     def get_isStand(self) -> bool:
         return self.__isStand
-    
+
     @property
     def get_isBurst(self) -> bool:
         return self.__isBurst
 
     def hit(self):
         pass
-    
+
     def stand(self) -> None:
         self.__isStand = True
 
     def burst(self) -> None:
-        if BLACK_JACK < self.__total:
+        if BLACK_JACK_NUMBER < self.__total:
             self.__isBurst = True
 
     def BlackJack(self) -> None:
-        if BLACK_JACK == self.__total:
+        if BLACK_JACK_NUMBER == self.__total:
             self.__isBlackJack = True
-
-    def Calculate_total(self) -> None:
-        total = 0
-        Ace_cards = []
-        for hand in self.__hand:
-            if hand.get_number != InitialCard.A:
-                total += hand.get_number
-            elif hand.get_number == InitialCard.A:
-                Ace_cards.append(hand)
-        if len(Ace_cards) != 0:
-            for hand in Ace_cards:
-                if BLACK_JACK < (self.__total + MAX_ACE):
-                    total += MIN_ACE
-                else:
-                    total += MAX_ACE
-        self.__total = total
-
 
 
 class Player(Rule):
@@ -124,7 +58,7 @@ class Player(Rule):
     @property
     def get_chip(self) -> int:
         return self.__chip
-    
+
     def bet(self, card):
         pass
 
@@ -136,5 +70,10 @@ class Dealer(Rule):
     def deal(self):
         pass
 
-deck = Deck()
-print(deck.deck)
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
